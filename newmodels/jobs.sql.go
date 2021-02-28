b565f6ba6f076da8e353c8397eebfcbdd0ebfbbe
Kyosuke95 <everlastingfriendship194@gmail.com>
2021-02-28 09:53:21 -0800

    sqlc.json: fix deprecated field name

--- newmodels/jobs.sql.go
+++ newmodels/jobs.sql.go
@@ -56,7 +56,9 @@ func (q *Queries) DeleteAllJobs(ctx context.Context) (int64, error) {
 }
 
 const getAllJobs = `-- name: GetAllJobs :many
-SELECT name, delivery_strategy, attempts, concurrency, created_at, auto_id FROM jobs WHERE name != 'meta.shutdown'
+SELECT name, delivery_strategy, attempts, concurrency, created_at, auto_id FROM jobs
+WHERE name != 'meta.shutdown'
+ORDER BY auto_id DESC
 `
 
 func (q *Queries) GetAllJobs(ctx context.Context) ([]Job, error) {
@@ -90,8 +92,7 @@ func (q *Queries) GetAllJobs(ctx context.Context) ([]Job, error) {
 }
 
 const getJob = `-- name: GetJob :one
-SELECT name, delivery_strategy, attempts, concurrency, created_at, auto_id
-FROM jobs
+SELECT name, delivery_strategy, attempts, concurrency, created_at, auto_id FROM jobs
 WHERE name = $1
 `
 
